b2a34d6a5a8ecee49bdc65c75e02e765ad43d98f
sahanan13 <sahanahome@gmail.com>
2021-07-20 13:20:53 -0700

    Added bottom navigation and created fragments for separate pages - home page, questionnaire page, and profile page

--- app/src/main/java/com/codepath/musicmix/MainActivity.java
+++ app/src/main/java/com/codepath/musicmix/MainActivity.java
@@ -2,6 +2,8 @@ package com.codepath.musicmix;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,48 +13,56 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.musicmix.models.Song;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*private TextView userView;
-    private TextView songView;
-    private Button addBtn;
-    private Song song;
+    private BottomNavigationView bottomNavigationView;
 
-    private SongService songService;
-    private ArrayList<Song> recentlyPlayedTracks;*/
-
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*songService = new SongService(getApplicationContext());
-        userView = (TextView) findViewById(R.id.user);
-        songView = (TextView) findViewById(R.id.song);
-        addBtn = (Button) findViewById(R.id.add);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
-        userView.setText(sharedPreferences.getString("userid", "No User"));
-
-        getTracks();
-
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                songService.addSongToLibrary(MainActivity.this.song);
-                if (recentlyPlayedTracks.size() > 0) {
-                    recentlyPlayedTracks.remove(0);
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        //fragment = new PostsFragment();
+                        break;
+                    case R.id.action_questionnaire:
+                        Toast.makeText(MainActivity.this, "Questionnaire!", Toast.LENGTH_SHORT).show();
+                        //fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        //fragment = new ProfileFragment();
+                        break;
                 }
-                updateSong();
+                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
-        });*/
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+
+
     }
 
     @Override
@@ -79,7 +89,74 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+}
+
+
+// Old MainActivity - to be deleted later
 /*
+public class MainActivity extends AppCompatActivity {
+
+    private TextView userView;
+    private TextView songView;
+    private Button addBtn;
+    private Song song;
+
+    private SongService songService;
+    private ArrayList<Song> recentlyPlayedTracks;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*songService = new SongService(getApplicationContext());
+        userView = (TextView) findViewById(R.id.user);
+        songView = (TextView) findViewById(R.id.song);
+        addBtn = (Button) findViewById(R.id.add);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        userView.setText(sharedPreferences.getString("userid", "No User"));
+
+        getTracks();
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                songService.addSongToLibrary(MainActivity.this.song);
+                if (recentlyPlayedTracks.size() > 0) {
+                    recentlyPlayedTracks.remove(0);
+                }
+                updateSong();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.btnLogout) {
+            // forget who's logged in
+            ParseUser.logOut();
+            ParseUser currentUser = ParseUser.getCurrentUser();
+
+            // navigate backwards to Login screen
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+            startActivity(i);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     private void getTracks() {
         songService.getRecentlyPlayedTracks(() -> {
             recentlyPlayedTracks = songService.getSongs();
@@ -92,5 +169,6 @@ public class MainActivity extends AppCompatActivity {
             songView.setText(recentlyPlayedTracks.get(0).getName());
             song = recentlyPlayedTracks.get(0);
         }
-    }*/
+    }
 }
+ */
